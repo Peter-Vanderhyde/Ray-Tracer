d8ea147626c929eda2c93c0d2add69a526875aae
Peter-Vanderhyde <peter.vanderhyde@gmail.com>
2023-08-13 05:29:16 -0600

    Error handling

--- save_details.h
+++ save_details.h
@@ -8,4 +8,4 @@ using byte = std::bitset<8>;
 
 std::optional<std::vector<std::bitset<8>>> get_details(const std::string& input_file, const int max_bits_possible);
 void save_details(const std::string& image_file, const std::optional<std::vector<std::bitset<8>>>& scene_details);
-std::vector<char> extract_details(const std::string &image_file);
+std::optional<std::vector<char>> extract_details(const std::string &image_file);
